502a26570594e1f88614165bf55f165b6937b16e
Competitve Calendar <competitivekalendar@gmail.com>
2021-08-09 01:18:12 +0530

    updated

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 curl http://18.117.101.101/api/contests > contests
 curl http://18.117.101.101/api/contests?platform=codechef > ./codechef/contests
-curl http://18.117.101.101/api/contests?platform=codechef&phase=upcoming > ./codechef/upcoming
-curl http://18.117.101.101/api/contests?platform=codechef&phase=ended > ./codechef/ended
-curl http://18.117.101.101/api/contests?platform=codechef&phase=running > ./codechef/running
+curl "http://18.117.101.101/api/contests?platform=codechef&phase=upcoming" > ./codechef/upcoming
+curl "http://18.117.101.101/api/contests?platform=codechef&phase=ended" > ./codechef/ended
+curl "http://18.117.101.101/api/contests?platform=codechef&phase=running" > ./codechef/running
 git add .
 git commit -m "updated"
 git push
