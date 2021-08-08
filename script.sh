98b149176edaec51f2ac6040c9700f94140167e3
Competitve Calendar <competitivekalendar@gmail.com>
2021-08-09 01:42:37 +0530

    updated

--- script.sh
+++ script.sh
@@ -1,8 +1,18 @@
-curl http://18.117.101.101/api/contests > contests
-curl http://18.117.101.101/api/contests?platform=codechef > ./codechef/contests
-curl "http://18.117.101.101/api/contests?platform=codechef&phase=upcoming" > ./codechef/upcoming
-curl "http://18.117.101.101/api/contests?platform=codechef&phase=ended" > ./codechef/ended
-curl "http://18.117.101.101/api/contests?platform=codechef&phase=running" > ./codechef/running
+ip="18.117.101.101"
+platforms=("codechef" "codeforces" "hackerearth" "hackerrank")
+phases=("upcoming" "running" "ended")
+curl "http://$ip/api/contests" > contests
+for i in {0..3}
+do
+curl "http://$ip/api/contests?platform=${platforms[$i]}" > ${platforms[$i]}/contests
+for j in {0..2}
+do
+curl "http://$ip/api/contests?platform=${platforms[$i]}&phase=${phases[$j]}" > ${platforms[$i]}/${phases[$j]}
+done
+done
+
+
+
 git add .
 git commit -m "updated"
 git push
