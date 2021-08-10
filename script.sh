011d756184e6595ed1a1f2b2eb9d6824c5e4bf2f
Competitve Calendar <competitivekalendar@gmail.com>
2021-08-10 12:28:56 +0530

    added universal upcoming and ended in the script

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ ip="18.117.101.101"
 platforms=("codechef" "codeforces" "hackerearth" "hackerrank")
 phases=("upcoming" "running" "ended")
 curl "http://$ip/api/contests" > contests
+curl "http://$ip/api/contests?phase=upcoming" > upcoming
+curl "http://$ip/api/contests?phase=ended" > ended
 for i in {0..3}
 do
 curl "http://$ip/api/contests?platform=${platforms[$i]}" > ${platforms[$i]}/contests
