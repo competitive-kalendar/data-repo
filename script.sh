e6cb5c3a27fa7b2119eb2e8b04fafc5d6527ede7
Competitve Calendar <competitivekalendar@gmail.com>
2021-09-01 18:35:11 +0530

    update running script

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 git pull
 
-ip="18.117.101.101"
+#ip="18.117.101.101"
 platforms=("codechef" "codeforces" "hackerearth" "hackerrank")
 phases=("upcoming" "running" "ended")
-curl "http://$ip/api/contests" > contests
-curl "http://$ip/api/contests?phase=upcoming" > upcoming
-curl "http://$ip/api/contests?phase=ended" > ended
-curl "http://$ip/api/contests?phase=running" > running
+curl "http://$ip/contests" > contests
+curl "http://$ip/contests?phase=upcoming" > upcoming
+curl "http://$ip/contests?phase=ended" > ended
+curl "http://$ip/contests?phase=running" > running
 for i in {0..3}
 do
-curl "http://$ip/api/contests?platform=${platforms[$i]}" > ${platforms[$i]}/contests
+curl "http://$ip/contests?platform=${platforms[$i]}" > ${platforms[$i]}/contests
 for j in {0..2}
 do
-curl "http://$ip/api/contests?platform=${platforms[$i]}&phase=${phases[$j]}" > ${platforms[$i]}/${phases[$j]}
+curl "http://$ip/contests?platform=${platforms[$i]}&phase=${phases[$j]}" > ${platforms[$i]}/${phases[$j]}
 done
 done
 
